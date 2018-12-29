16d6e4af3448d501eab4ba2a4c5ee90b54110dfd
CSnowStack <csnowstack@gmail.com>
2018-12-29 15:52:36 +0800

    init

---
+++ app/src/main/java/csnowstack/studynote/MainActivity.java
@@ -0,0 +1,13 @@
+package csnowstack.studynote;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
